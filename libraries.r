cbbd9cf6f19acad2a3e13a678b2adde69badb2e1
aelb66 <75398560+aelb66@users.noreply.github.com>
2021-07-01 18:31:36 +1000

    Create libraries.r

---
+++ libraries.r
@@ -0,0 +1,15 @@
+```{r libraries}
+library(nhanesA) #importing NHANES data
+library(dplyr) #manipulating data
+library(purrr) #manipulating data
+library(finalfit) #initial data analysis
+library(caret) #ML workflows
+library(randomForest)#Random Forest
+library(naivebayes) #Naivebayes
+library(glmnet)# LASSO regression
+library(ggplot2)#visualisation
+library(mice) #univariate imputation
+library(NADIA) #reusing same univariate imputation on test data
+library(plyr) #XGBoost
+library(xgboost)#XGBoost
+```
